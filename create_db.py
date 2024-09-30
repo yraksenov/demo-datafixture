03ae084ecf1325b068d6bed1342b4f10b24dd368
Yury Aksenov <yraksenov@gmail.com>
2024-09-30 20:27:41 +0300

    Added a data pipeline.

--- create_db.py
+++ create_db.py
@@ -4,11 +4,21 @@ import sqlite3
 import pandas as pd
 
 
-SQL_CREATE = """
-    CREATE TABLE IF NOT EXISTS TEST (
+SQL_CREATE_INPUT = """
+    CREATE TABLE IF NOT EXISTS INPUT (
         v1 INT NOT NULL, 
         op CHAR(1) NOT NULL,
         v2 INT NOT NULL
+    );
+"""
+
+
+SQL_CREATE_OUTPUT = """
+    CREATE TABLE IF NOT EXISTS OUTPUT (
+        v1 INT NOT NULL, 
+        op CHAR(1) NOT NULL,
+        v2 INT NOT NULL,
+        result REAL NOT NULL
     );
 """
 
@@ -17,10 +27,11 @@ def main(database_path):
     df = pd.read_csv(os.path.join("data_fixtures", "input_data.csv"))
     with sqlite3.connect(database_path) as connection:
         cursor = connection.cursor()
-        cursor.execute(SQL_CREATE)
+        cursor.execute(SQL_CREATE_INPUT)
+        cursor.execute(SQL_CREATE_OUTPUT)
         connection.commit()
         df["v1 op v2".split()].to_sql(
-            "TEST", connection, index=False, if_exists="append"
+            "INPUT", connection, index=False, if_exists="append"
         )
 
 
