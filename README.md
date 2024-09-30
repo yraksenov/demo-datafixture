2ae0a7d6958a80dea37fbdb15cee4a1eda93c722
Yury Aksenov <yraksenov@gmail.com>
2024-09-30 18:56:42 +0300

    Added data generator.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
+This is a demo project. 
+It demonstrates the use of replacement for the Extraction layer with a test fixture. 
+
 # demo-datafixture
+
+You will need Poetry tool for dependency management. Install it using the [Poetry Installation Guide](https://python-poetry.org/docs/#installation) .
+
+First, use ``poetry env use `which python` `` to set up Python enterperter you are using.
+
+Second, use ``poetry install`` to collect dependencies for the environment.
+
+Thrid, use ``poetry shell`` to enter the environment.  
+
+
+Now you may run data_gen: ``python data_gen.py`` to create the data fixtures directory with input data and expectations files.
+
---
+++ data_gen.py
@@ -0,0 +1,28 @@
+import os
+import random
+
+import pandas as pd
+
+
+def row_generator(count):
+    operations = "+-/*"
+    for _ in range(count):
+        val1 = random.randint(1, 100)
+        operation = random.choice(operations)
+        val2 = random.randint(1, 100)
+        yield val1, operation, val2, eval(f"{val1} {operation} {val2}")
+
+
+def main(fixtures_dir, count):
+    df = pd.DataFrame(row_generator(count), columns="v1 op v2 expectation".split())
+    expectations_filename = os.path.join(fixtures_dir, "expectations.csv")
+    input_data_filename = os.path.join(fixtures_dir, "input_data.csv")
+    df.to_csv(expectations_filename)
+    df["v1 op expectation".split()].to_csv(input_data_filename)
+
+
+if __name__ == "__main__":
+    fixtures_dir = "data_fixtures"
+    os.makedirs(fixtures_dir, exist_ok=True)
+    main(fixtures_dir, 1000)
+    print(fixtures_dir, "directory successfully created.")
